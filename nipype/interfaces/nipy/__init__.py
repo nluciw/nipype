c088836493a273494aee0981909ad1b727b98ff4
Nicholas Luciw <nicholas.luciw@mail.utoronto.ca>
2019-03-21 16:39:35 -0400

    Add Trim to imports of __init__

--- nipype/interfaces/nipy/__init__.py
+++ nipype/interfaces/nipy/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from .model import FitGLM, EstimateContrast
-from .preprocess import ComputeMask, SpaceTimeRealigner
+from .preprocess import ComputeMask, SpaceTimeRealigner, Trim
 from .utils import Similarity
